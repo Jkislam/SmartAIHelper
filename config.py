128acd2ce866b13c77e692bcff74b25818a48611
Jkislam <155318094+Jkislam@users.noreply.github.com>
2025-08-02 00:17:06 +0600

    Add files via upload

---
+++ config.py
@@ -0,0 +1,2 @@
+# এখানে আপনার OpenAI API key বসান
+API_KEY = "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"  # <-- এখানে আপনার key বসান
